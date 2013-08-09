d076a04a8135fb388c0d0e5ae36f3393a36c1c17
Dan Nixon <dan@dan-nixon.com>
2013-08-09 16:19:01 +0100

    Removed old files

--- mjpg-streamer/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd mjpg-streamer
-export LD_LIBRARY_PATH=./
-./mjpg_streamer -i "input_file.so -f ../" -o "output_http.so -w ./www"
--- stop_timelapse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/stream_tl
-
-echo "Stopping capture"
-sudo killall streaming_timelapse
-echo "Stopping MJPEG streamer"
-pkill -SIGINT mjpg_streamer
