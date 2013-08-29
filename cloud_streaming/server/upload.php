bb397f075d59301c66a1a7a9e2ce46086d112359
Dan Nixon <dan@dan-nixon.com>
2013-08-29 17:02:25 +0100

    Updated php script to do unwrap

--- cloud_streaming/server/upload.php
+++ cloud_streaming/server/upload.php
@@ -6,8 +6,10 @@ if($_POST['key'] != $key) {
     die("AUTH_FAIL");
 }
 
-if (move_uploaded_file($_FILES['frame']['tmp_name'], "/var/www/frame/i.jpg")) {
+if (move_uploaded_file($_FILES['frame']['tmp_name'], "/var/www/d.jpg")) {
     echo "FRAME_UPLOAD";
+    exec("/var/www/equi_gen /var/www/d.jpg /var/www/frame/i.jpg");
+    exec("rm /var/www/d.jpg");
     chmod("/var/www/frame/i.jpg", 0777);
 } else {
     die("FRAME_FAIL");
