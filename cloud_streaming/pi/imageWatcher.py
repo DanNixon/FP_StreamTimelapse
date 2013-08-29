f4c8b9be7fae11ec029d13f4039484b0c6911a61
Dan Nixon <dan@dan-nixon.com>
2013-08-29 14:11:14 +0100

    imageWriter.py now prints less

--- cloud_streaming/pi/imageWatcher.py
+++ cloud_streaming/pi/imageWatcher.py
@@ -10,11 +10,15 @@ def upload_handler():
 	global upload_count
 	local_file = "frame{0}.jpg".format(upload_count)
 	upload_count += 1
+	if upload_count % 50 == 0:
+		print "imageWatcher: image_{0}".format(upload_count)
 	shutil.copyfile(frame_filename, local_file)
 	register_openers()
 	datagen, headers = multipart_encode({"frame" : open(local_file, "rb"), "key" : service_key})
-	request = urllib2.Request("http://[SERVER_IP]/upload.php", datagen, headers)
-	print urllib2.urlopen(request).read()
+	request = urllib2.Request("http://[IP_HOST]/upload.php", datagen, headers)
+	result = urllib2.urlopen(request).read()
+	if result != "FRAME_UPLOAD":
+		print result
 	os.remove(local_file)
 
 wm = pyinotify.WatchManager()
@@ -23,9 +27,7 @@ mask = pyinotify.IN_CLOSE_WRITE
 class EventHandler(pyinotify.ProcessEvent):
 	def process_IN_CLOSE_WRITE(self, event):
 		if event.pathname == frame_filename:
-			print "New image"
 			upload_handler()
-
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
