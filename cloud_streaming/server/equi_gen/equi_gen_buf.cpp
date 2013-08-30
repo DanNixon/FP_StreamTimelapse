bd00255ae433c405ec3752cf1ca0fa0b58aa2b39
Dan Nixon <dan@dan-nixon.com>
2013-08-30 12:48:39 +0100

    Updated to new equi_gen app

--- cloud_streaming/server/equi_gen/equi_gen_buf.cpp
+++ cloud_streaming/server/equi_gen/equi_gen_buf.cpp
@@ -8,23 +8,21 @@
 #include <cv.h>
 #include <highgui.h>
 
-#include "equi_gen.h"
+#include "equi_gen_buf.h"
 
-void createEquiRectangularUnwrap(char *src_file,
-        char *dest_file, int width,
-        float uPerpendicularCentre, float vPerpendicularCentre,
-        float minDiameter, float maxDiameter,
-        float angleOffsetRadians, float aspect)
+cv::Mat createEquiRectangularUnwrap(cv::Mat image_in,
+                                 int width,
+                                 float uPerpendicularCentre, float vPerpendicularCentre,
+                                 float minDiameter, float maxDiameter,
+                                 float angleOffsetRadians, float aspect)
 {
-    IplImage *image_in = cvLoadImage(src_file, CV_LOAD_IMAGE_COLOR);
-    assert((image_in->nChannels) = 3);
-
     float minRadius = minDiameter * 0.5f;
     float maxRadius = maxDiameter * 0.5f;
     int fullHeight = (int) (width / (2 * M_PI));
 
-    unsigned char* pixels = malloc((fullHeight * width) * 3);
-    unsigned char* oldPixels = image_in->imageData;
+    int data_size = (fullHeight * width) * 3;
+    unsigned char* pixels = new unsigned char[data_size];
+    unsigned char* oldPixels = image_in.data;
 
     int index = 0;
     int i, j;
@@ -47,10 +45,10 @@ void createEquiRectangularUnwrap(char *src_file,
             u += uPerpendicularCentre;
             v += (1.0f - vPerpendicularCentre);
 
-            int xPixel = (int) ((1 - v) * image_in->width);
-            int yPixel = (int) ((1 - u) * image_in->height);
+            int xPixel = (int) ((1 - v) * image_in.cols);
+            int yPixel = (int) ((1 - u) * image_in.rows);
 
-            int oldPixelIndex = ((yPixel * (image_in->width)) + xPixel) * 3;
+            int oldPixelIndex = ((yPixel * (image_in.cols)) + xPixel) * 3;
 
             pixels[index] = oldPixels[oldPixelIndex];
             pixels[index + 1] = oldPixels[oldPixelIndex + 1];
@@ -60,11 +58,7 @@ void createEquiRectangularUnwrap(char *src_file,
         }
     }
 
-    CvSize new_size;
-    new_size.width = width;
-    new_size.height = fullHeight;
-    IplImage *new_image = cvCreateImageHeader(new_size, image_in->depth, 3);
-    cvSetData(new_image, pixels, CV_AUTOSTEP);
+    cv::Mat image_out = cv::Mat(fullHeight, width, CV_8UC3, pixels);
 
-    cvSaveImage(dest_file, new_image, 0);
+    return image_out;
 }
