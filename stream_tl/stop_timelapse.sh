f9a0b99ddade3dfbc1cbd1007d188bae47342695
Dan Nixon <dan@dan-nixon.com>
2013-09-02 11:07:29 +0100

    Added note in stop_timelapse.sh

--- stream_tl/stop_timelapse.sh
+++ stream_tl/stop_timelapse.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+##Expect this to throw out errors or usage docs if called without a (complete) timelapse running
+
 cd /home/pi/stream_tl
 
 rm ./gps.temp
@@ -16,6 +18,5 @@ sudo kill $(ps h --ppid $(screen -ls | grep gps | cut -d. -f1) -o pid)
 echo "Killing gpio watcher"
 sudo kill $(ps h --ppid $(screen -ls | grep button_watcher | cut -d. -f1) -o pid)
 
-#
 echo "Removing queued streaming frames"
 rm frame*.jpg
