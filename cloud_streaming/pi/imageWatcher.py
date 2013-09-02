02efbce39f5399beaf87d6099706abc6cb335259
Dan Nixon <dan@dan-nixon.com>
2013-09-02 09:47:50 +0100

    Fixed imageWatcher
    Will no longer die on network errors.
    Allows continuing of streaming after network connect has been recovered without restarting timelaspe capture.

--- cloud_streaming/pi/imageWatcher.py
+++ cloud_streaming/pi/imageWatcher.py
@@ -7,33 +7,37 @@ frame_filename = "/home/pi/stream_tl/s_frame.jpg"
 service_key = "FPST_CLOUD_DEV"
 
 def upload_handler():
-	global upload_count
-	local_file = "frame{0}.jpg".format(upload_count)
-	upload_count += 1
-	if upload_count % 50 == 0:
-		print "imageWatcher: image_{0}".format(upload_count)
-	shutil.copyfile(frame_filename, local_file)
-	register_openers()
-	gps_file = open("./gps.temp")
-	gps_string = gps_file.read()
-	gps_file.close()
-	gps_data = gps_string.split()
-	post_data = {"frame" : open(local_file, "rb"), "key" : service_key}
 	try:
-		post_data["lat"] = gps_data[0]
-		post_data["lon"] = gps_data[1]
-		post_data["alt"] = gps_data[2]
-		post_data["track"] = gps_data[3]
-		post_data["speed"] = gps_data[4]
-	except IndexError:
-		print "error"
+		global upload_count
+		local_file = "frame{0}.jpg".format(upload_count)
+		upload_count += 1
+		if upload_count % 50 == 0:
+			print "imageWatcher: image_{0}".format(upload_count)
+		shutil.copyfile(frame_filename, local_file)
+		register_openers()
+		gps_file = open("./gps.temp")
+		gps_string = gps_file.read()
+		gps_file.close()
+		gps_data = gps_string.split()
+		post_data = {"frame" : open(local_file, "rb"), "key" : service_key}
+		try:
+			post_data["lat"] = gps_data[0]
+			post_data["lon"] = gps_data[1]
+			post_data["alt"] = gps_data[2]
+			post_data["track"] = gps_data[3]
+			post_data["speed"] = gps_data[4]
+		except IndexError:
+			print "GPS Error"
+			pass
+		datagen, headers = multipart_encode(post_data)
+		request = urllib2.Request("http://[IP_HOST]/upload.php", datagen, headers)
+		result = urllib2.urlopen(request).read()
+		if result != "FRAME_UPLOAD":
+			print result
+		os.remove(local_file)
+	except urllib2.URLError:
+		print "Network Error"
 		pass
-	datagen, headers = multipart_encode(post_data)
-	request = urllib2.Request("http://[HOST_IP]/upload.php", datagen, headers)
-	result = urllib2.urlopen(request).read()
-	if result != "FRAME_UPLOAD":
-		print result
-	os.remove(local_file)
 
 wm = pyinotify.WatchManager()
 mask = pyinotify.IN_CLOSE_WRITE
