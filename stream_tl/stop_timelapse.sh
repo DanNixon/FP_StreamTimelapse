c4563ce447637ce667df15b6ed394bffd4a6f4f6
Dan Nixon <dan@dan-nixon.com>
2013-09-02 12:07:25 +0100

    Fixed time skew bug
    Using multiple RaspiStill commands to get streaming frames, results in lower streaming frame rate but more accurate timelapse delays.

--- stream_tl/stop_timelapse.sh
+++ stream_tl/stop_timelapse.sh
@@ -15,7 +15,7 @@ pkill -SIGINT mjpg_streamer
 echo "Killing Python"
 sudo kill $(ps h --ppid $(screen -ls | grep gps | cut -d. -f1) -o pid)
 
-echo "Killing gpio watcher"
+echo "Killing GPIO watcher"
 sudo kill $(ps h --ppid $(screen -ls | grep button_watcher | cut -d. -f1) -o pid)
 
 echo "Removing queued streaming frames"
