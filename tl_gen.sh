006c867508969f177c5a1212c5f1aeeb36d776ea
Dan Nixon <dan@dan-nixon.com>
2013-09-05 11:08:31 +0100

    Improved file name handling

--- tl_gen.sh
+++ tl_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ffmpeg -f image2 -r 10 -i $1 -vcodec libx264 -preset ultrafast -crf 25 $2
+ffmpeg -f image2 -r 10 -i "$1" -vcodec libx264 -preset ultrafast -crf 25 "$2"
 
 #-r FRAME RATE
 #-crf QUALITY
