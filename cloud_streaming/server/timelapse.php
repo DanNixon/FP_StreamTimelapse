556c2c18f8f7b719acf77a3d169ee7ea8722a1d9
Dan Nixon <dan@dan-nixon.com>
2013-09-09 15:01:13 +0100

    Added frame window options to timelapse video export

--- cloud_streaming/server/timelapse.php
+++ cloud_streaming/server/timelapse.php
@@ -18,16 +18,61 @@ function check_file() {
 	}
 }
 
+function send_tl_request(tl_path) {
+	var url = "http://37.139.30.37/timelapse.php?";
+	url += ("tl=" + tl_path);
+	url += ("&l_lim=" + document.getElementById("l-lim").value);
+	url += ("&u_lim=" + document.getElementById("u-lim").value);
+	console.log(url);
+	window.location.replace(url);
+}
+
+function reg_events() {
+	var original_gen = document.getElementById("gen_orig");
+	if(original_gen.addEventListener) {
+		original_gen.addEventListener("click", function() {
+			send_tl_request("/i%d.jpg");
+		});
+	} else {
+		original_gen.attachEvent("click", function() {
+			send_tl_request("/i%d.jpg");
+		});
+	}
+	
+	var equi_gen = document.getElementById("gen_equi");
+	if(equi_gen.addEventListener) {
+		equi_gen.addEventListener("click", function() {
+			send_tl_request("/i%d_e.jpg");
+		});
+	} else {
+		equi_gen.attachEvent("click", function() {
+			send_tl_request("/i%d_e.jpg");
+		});
+	}
+}
+
 self.setInterval(function(){check_file()}, 1000);
+
+if(window.addEventListener) {
+	window.addEventListener("load", reg_events, false);
+} else if(window.attachEvent) {
+	window.attachEvent("onload", reg_events);
+} else {
+	document.addEventListener("load", reg_events, false);
+}
 </script>
 </head>
 <body>
-<p><a href="timelapse.php?tl=/i%d.jpg">Generate Orignal Timelapse</a></p>
-<p><a href="timelapse.php?tl=/i%d_e.jpg">Generate Equi Timelapse</a></p>
+<p>
+	Lower frame limit: <input type="text" size="3" id="l-lim" value="0">
+	Upper frame limit: <input type="text" size="3" id="u-lim" value="1000">
+</p>
+<p id="gen_orig">Generate Orignal Timelapse</p>
+<p id="gen_equi">Generate Equi Timelapse</p>
 <?php
 if(isset($_GET['tl'])) {
 	$tl_filename = sprintf("tl_%d.mp4", time());
-	exec('/var/www/tl_gen.sh /var/www/tl_frames'.$_GET['tl'].' "/var/www/tl_exports/'.$tl_filename.'" > /dev/null &');
+	exec('/var/www/tl_gen.sh '.$_GET['l_lim'].' '.$_GET['u_lim'].' /var/www/tl_frames'.$_GET['tl'].' "/var/www/tl_exports/'.$tl_filename.'" > /dev/null &');
 	echo("<p id='filename'>".$tl_filename."</p>");
 }
 ?>
