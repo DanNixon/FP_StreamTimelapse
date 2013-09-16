8f9817fdc4ec961e6d3d531dd767ab0992285f8b
Dan Nixon <dan@dan-nixon.com>
2013-09-16 11:33:14 +0100

    Allow time and date to fail over to EXIF if GPS not set

--- cloud_streaming/server/get_image_gps.php
+++ cloud_streaming/server/get_image_gps.php
@@ -1,5 +1,4 @@
 <?php
-
 if(isset($_GET['frame'])) {
 	get_exif($_GET['frame']);
 } else {
@@ -10,13 +9,26 @@ function get_exif($frame_url) {
 	$exif = exif_read_data($frame_url, 0, true);
 	$gps = $exif['GPS'];
 
+	$time_string = $gps['GPSTimeStamp'];
+	$date_string = $gps['GPSDateStamp'];
+	
+	if(empty($time_string) or empty($date_string)) {
+		$dt_string = $exif['EXIF']['DateTimeOriginal'];
+		$td_data = explode(":", $dt_string);
+		
+		//TODO: This will need changed when https://github.com/raspberrypi/userland/pull/85 is merged
+		$time = sprintf("%'02d:%'02d:%'02d", $td_data[3], $td_data[4], $td_data[5]);
+		$date = sprintf("%'02d/%'02d/%'04d", $td_data[2], $td_data[1], $td_data[0]);
+	} else {
+		$time = timestamp_to_string($time_string);
+		$date = datestamp_to_string($date_string);
+	}
+
 	$lat = dms_to_decimal($gps['GPSLatitude'], $gps['GPSLatitudeRef']);
 	$lon = dms_to_decimal($gps['GPSLongitude'], $gps['GPSLongitudeRef']);
 	$track = rational_to_decimal($gps['GPSTrack']);
 	$alt = rational_to_decimal($gps['GPSAltitude']);
 	$speed = rational_to_decimal($gps['GPSSpeed']);
-	$time = timestamp_to_string($gps['GPSTimeStamp']);
-	$date = datestamp_to_string($gps['GPSDateStamp']);
 
 	$json_string = sprintf('{"lat":%F,"lon":%F,"alt":%F,"track":%F,"speed":%F,"time":"%s","date":"%s"}', $lat, $lon, $alt, $track, $speed, $time, $date);
 	echo($json_string);
